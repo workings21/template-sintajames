91f17bc6302aa3ba556d7493ceceb12c50a1e82f
Rizki Darmawan <rizkidarmawan.0402102@gmail.com>
2022-05-31 23:49:30 +0700

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        cookie: ['Cookie'],
+        tiro: ['Tiro Telugu'],
+      },
+    },
+  },
+  plugins: [],
+}
